2cc5724a5a98324e8bd99cd941a0e612604b1a3d
Miquel Massot <miquel.massot@gmail.com>
2023-06-01 16:58:47 +0100

    Bump to v1.0.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyvoyis",
-    version="1.0.0",
+    version="1.0.2",
     description="Voyis Recon LS python driver",
     long_description=long_description,
     long_description_content_type="text/markdown",
